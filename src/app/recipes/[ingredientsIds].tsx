e7a16cb54ff01bf7f8418cac2c5d98e0b3ac6cad
MichelleCordeiro <mic321@gmail.com>
2024-03-09 00:37:39 -0300

    feat: ensure selected ingredients go to recipes page

--- src/app/recipes/[ingredientsIds].tsx
+++ src/app/recipes/[ingredientsIds].tsx
@@ -1,17 +1,32 @@
 import { FlatList, Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
 import { MaterialIcons } from '@expo/vector-icons'
-import { router } from 'expo-router'
-import { Recipe } from '@/components/Recipe'
+import { router, useLocalSearchParams } from 'expo-router'
 
+import { Recipe } from '@/components/Recipe'
+import { services } from '@/services'
 import { styles } from './styles'
+import { Ingredients } from '@/components/Ingredients'
 
 export default function Recipes() {
+  const [ingredients, setIngredients] = useState<IngredientResponse[]>([])
+  const params = useLocalSearchParams<{ ingredientsIds: string }>()
+  // console.log(params)
+
+  const ingredientsIds = params.ingredientsIds.split(',')
+
+  useEffect(() => {
+    services.ingredientes.findByIds(ingredientsIds).then(setIngredients);
+  },[])
+  
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <MaterialIcons name='arrow-back' size={32} onPress={() => router.back()} />
 
         <Text style={styles.title}>Ingredientes</Text>
+
+        <Ingredients ingredients={ingredients} />
 
         <FlatList
           data={['1']}
