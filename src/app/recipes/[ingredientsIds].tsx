313b4bfc7d1d72b93696e2b52bcb666ec5b4516f
MichelleCordeiro <mic321@gmail.com>
2024-03-09 01:26:19 -0300

    feat: add recipe page

--- src/app/recipes/[ingredientsIds].tsx
+++ src/app/recipes/[ingredientsIds].tsx
@@ -37,7 +37,11 @@ export default function Recipes() {
       <FlatList
         data={recipes}
         keyExtractor={item => item.id}
-        renderItem={({ item }) => <Recipe recipe={item} />}
+        renderItem={({ item }) => 
+          <Recipe 
+            recipe={item} 
+            onPress={() => router.navigate('/recipe/' + item.id)} 
+          />}
         style={styles.recipes}
         contentContainerStyle={styles.recipesContent}
         showsVerticalScrollIndicator={false}
